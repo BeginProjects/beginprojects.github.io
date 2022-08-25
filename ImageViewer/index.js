1d532ae795501ff9af62ad009b7c6714fa1550fd
BeginProjects <111580459+BeginProjects@users.noreply.github.com>
2022-08-25 16:00:40 +0530

    Update index.js

--- ImageViewer/index.js
+++ ImageViewer/index.js
@@ -1,5 +1,5 @@
 let image = document.getElementById('img-box')
-let img_path = "/home/darklinux/Desktop/Coding Practice/WebDevelopment/DashProjects/PersonalProjects/ImageViewer/Images"
+let img_path = "/ImageViewer/Images"
 let img_list = [`${img_path}/01.jpg`, `${img_path}/02.jpg`,`${img_path}/03.jpg`,`${img_path}/04.jpg`,`${img_path}/05.jpg`,`${img_path}/06.jpg`,`${img_path}/07.jpg`,`${img_path}/08.jpg`,`${img_path}/09.jpg`,`${img_path}/10.jpg`,`${img_path}/11.jpg`]
 let current_index
 let n = -1
